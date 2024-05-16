3184a42852505c6f3dab91af51204a37386f313c
Prost333 <127540780+Prost333@users.noreply.github.com>
2024-05-16 16:29:41 +0300

    Update Type.java

--- src/main/java/com/aston/intensive/hibernate/astonintensivehibernate/model/enums/Type.java
+++ src/main/java/com/aston/intensive/hibernate/astonintensivehibernate/model/enums/Type.java
@@ -1,5 +1,5 @@
 package com.aston.intensive.hibernate.astonintensivehibernate.model.enums;
-
+// тут кнш твоя логика но может, стоило бы подумать об объединении всех типов энамов в один класс или объединить некоторые как countCode countName
 public enum Type {
     FRUIT,;
 }
