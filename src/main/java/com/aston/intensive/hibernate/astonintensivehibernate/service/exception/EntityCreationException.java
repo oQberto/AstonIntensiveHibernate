b84ed24e4f0c2a4ea08627b7a943e0d0ebb58d6a
Prost333 <127540780+Prost333@users.noreply.github.com>
2024-05-16 17:01:30 +0300

    Update EntityCreationException.java

--- src/main/java/com/aston/intensive/hibernate/astonintensivehibernate/service/exception/EntityCreationException.java
+++ src/main/java/com/aston/intensive/hibernate/astonintensivehibernate/service/exception/EntityCreationException.java
@@ -1,5 +1,5 @@
 package com.aston.intensive.hibernate.astonintensivehibernate.service.exception;
-
+// тут дело вкуса, но я бы вынес все исключения в отдельную папку и сделал бы хендлер 
 public class EntityCreationException extends RuntimeException {
 
     public EntityCreationException(String message) {
